b563d899aa03ab346d0fd8b3843926c4e3052725
msingi <gobaseyourself@gmail.com>
2021-01-11 13:14:23 -0600

    Add events, cascadeV1 setter, more input checks, make public functions external, add natspec comments

--- contracts/CascadeV2.sol
+++ contracts/CascadeV2.sol
@@ -3,14 +3,16 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 import "./BaseToken.sol";
-import "./Cascade.sol";
 
+/**
+ * @title CascadeV2 is a liquidity mining contract.
+ */
 contract CascadeV2 is OwnableUpgradeSafe {
     using SafeMath for uint256;
 
-    mapping(address => uint256[]) public userDeposits_numLPTokens;
-    mapping(address => uint256[]) public userDeposits_depositTimestamp;
-    mapping(address => uint8[])   public userDeposits_multiplierLevel;
+    mapping(address => uint256[]) public userDepositsNumLPTokens;
+    mapping(address => uint256[]) public userDepositsDepositTimestamp;
+    mapping(address => uint8[])   public userDepositsMultiplierLevel;
     mapping(address => uint256)   public userTotalLPTokensLevel1;
     mapping(address => uint256)   public userTotalLPTokensLevel2;
     mapping(address => uint256)   public userTotalLPTokensLevel3;
@@ -29,16 +31,24 @@ contract CascadeV2 is OwnableUpgradeSafe {
 
     IERC20 public lpToken;
     BaseToken public BASE;
-    Cascade public cascadeV1;
+    address public cascadeV1;
 
     event Deposit(address indexed user, uint256 tokens, uint256 timestamp);
     event Withdraw(address indexed user, uint256 withdrawnLPTokens, uint256 withdrawnBASETokens, uint256 timestamp);
     event UpgradeMultiplierLevel(address indexed user, uint256 depositIndex, uint256 oldLevel, uint256 newLevel, uint256 timestamp);
     event Migrate(address indexed user, uint256 lpTokens, uint256 rewardTokens);
     event AddRewards(uint256 tokens, uint256 shares, uint256 vestingStart, uint256 vestingDuration, uint256 totalTranches);
+    event SetBASEToken(address token);
+    event SetLPToken(address token);
+    event SetCascadeV1(address cascadeV1);
+    event UpdateDepositSeconds(address user, uint256 totalDepositSeconds, uint256 userDepositSeconds);
+    event AdminRescueTokens(address token, address recipient, uint256 amount);
 
+    /**
+     * @dev Called by the OpenZeppelin "upgrades" library to initialize the contract in lieu of a constructor.
+     */
     function initialize()
-        public
+        external
         initializer
     {
         __Ownable_init();
@@ -48,35 +58,78 @@ contract CascadeV2 is OwnableUpgradeSafe {
      * Admin
      */
 
+    /**
+     * @notice Changes the address of the LP token for which staking is allowed.
+     * @param _lpToken The address of the LP token.
+     */
     function setLPToken(address _lpToken)
-        public
+        external
         onlyOwner
     {
+        require(_lpToken != address(0x0), "zero address");
         lpToken = IERC20(_lpToken);
+        emit SetLPToken(_lpToken);
     }
 
+    /**
+     * @notice Changes the address of the BASE token.
+     * @param _baseToken The address of the BASE token.
+     */
     function setBASEToken(address _baseToken)
-        public
+        external
         onlyOwner
     {
+        require(_baseToken != address(0x0), "zero address");
         BASE = BaseToken(_baseToken);
+        emit SetBASEToken(_baseToken);
     }
 
-    function adminRescueTokens(address token, address recipient, uint256 amount)
-        public
+    /**
+     * @notice Changes the address of Cascade v1 (for purposes of migration).
+     * @param _cascadeV1 The address of Cascade v1.
+     */
+    function setCascadeV1(address _cascadeV1)
+        external
         onlyOwner
     {
+        require(_cascadeV1 != address(0x0), "zero address");
+        cascadeV1 = _cascadeV1;
+        emit SetCascadeV1(_cascadeV1);
+    }
+
+    /**
+     * @notice Allows the admin to withdraw tokens mistakenly sent into the contract.
+     * @param token The address of the token to rescue.
+     * @param recipient The recipient that the tokens will be sent to.
+     * @param amount How many tokens to rescue.
+     */
+    function adminRescueTokens(address token, address recipient, uint256 amount)
+        external
+        onlyOwner
+    {
+        require(token != address(0x0), "zero address");
         require(recipient != address(0x0), "bad recipient");
-        require(amount > 0, "bad amount");
+        require(amount > 0, "zero amount");
 
         bool ok = IERC20(token).transfer(recipient, amount);
         require(ok, "transfer");
+
+        emit AdminRescueTokens(token, recipient, amount);
     }
 
+    /**
+     * @notice Allows the owner to add another tranche of rewards.
+     * @param numTokens How many tokens to add to the tranche.
+     * @param vestingStart The timestamp upon which vesting of this tranche begins.
+     * @param vestingDuration The duration over which the tokens fully unlock.
+     */
     function addRewards(uint256 numTokens, uint256 vestingStart, uint256 vestingDuration)
-        public
+        external
         onlyOwner
     {
+        require(numTokens > 0, "zero amount");
+        require(vestingStart > 0, "zero vesting start");
+
         uint256 numShares = tokensToShares(numTokens);
         rewards_numShares.push(numShares);
         rewards_vestingStart.push(vestingStart);
@@ -92,19 +145,25 @@ contract CascadeV2 is OwnableUpgradeSafe {
      * Public methods
      */
 
+    /**
+     * @notice Allows a user to deposit LP tokens into the Cascade.
+     * @param amount How many tokens to stake.
+     */
     function deposit(uint256 amount)
-        public
+        external
     {
-        updateDepositSeconds(msg.sender);
+        require(amount > 0, "zero amount");
 
         uint256 allowance = lpToken.allowance(msg.sender, address(this));
         require(amount <= allowance, "allowance");
 
+        updateDepositSeconds(msg.sender);
+
         totalDepositedLevel1 = totalDepositedLevel1.add(amount);
         userTotalLPTokensLevel1[msg.sender] = userTotalLPTokensLevel1[msg.sender].add(amount);
-        userDeposits_numLPTokens[msg.sender].push(amount);
-        userDeposits_depositTimestamp[msg.sender].push(now);
-        userDeposits_multiplierLevel[msg.sender].push(1);
+        userDepositsNumLPTokens[msg.sender].push(amount);
+        userDepositsDepositTimestamp[msg.sender].push(now);
+        userDepositsMultiplierLevel[msg.sender].push(1);
 
         bool ok = lpToken.transferFrom(msg.sender, address(this), amount);
         require(ok, "transferFrom");
@@ -112,9 +171,15 @@ contract CascadeV2 is OwnableUpgradeSafe {
         emit Deposit(msg.sender, amount, now);
     }
 
+    /**
+     * @notice Allows a user to withdraw LP tokens from the Cascade.
+     * @param amount How many tokens to unstake.
+     */
     function withdrawLPTokens(uint256 amount)
-        public
+        external
     {
+        require(amount > 0, "zero amount");
+
         updateDepositSeconds(msg.sender);
 
         uint256 totalAmountToWithdraw;
@@ -122,20 +187,20 @@ contract CascadeV2 is OwnableUpgradeSafe {
         uint256 amountToWithdrawLevel1;
         uint256 amountToWithdrawLevel2;
         uint256 amountToWithdrawLevel3;
-        for (uint256 i = userDeposits_numLPTokens[msg.sender].length - 1; i >= 0; i++) {
+        for (uint256 i = userDepositsNumLPTokens[msg.sender].length - 1; i >= 0; i++) {
             uint256 lpTokens;
             if (totalAmountToWithdraw.add(lpTokens) < amount) {
-                lpTokens = userDeposits_numLPTokens[msg.sender][i];
-                userDeposits_numLPTokens[msg.sender].pop();
-                userDeposits_depositTimestamp[msg.sender].pop();
+                lpTokens = userDepositsNumLPTokens[msg.sender][i];
+                userDepositsNumLPTokens[msg.sender].pop();
+                userDepositsDepositTimestamp[msg.sender].pop();
             } else {
                 lpTokens = amount.sub(totalAmountToWithdraw);
-                userDeposits_numLPTokens[msg.sender][i] = lpTokens.sub(lpTokens);
+                userDepositsNumLPTokens[msg.sender][i] = lpTokens.sub(lpTokens);
             }
 
             uint256 depositSecondsToBurn;
-            uint256 age = userDeposits_depositTimestamp[msg.sender][i];
-            uint8 multiplier = userDeposits_multiplierLevel[msg.sender][i];
+            uint256 age = userDepositsDepositTimestamp[msg.sender][i];
+            uint8 multiplier = userDepositsMultiplierLevel[msg.sender][i];
             if (multiplier == 1) {
                 userTotalLPTokensLevel1[msg.sender] = userTotalLPTokensLevel1[msg.sender].sub(lpTokens);
                 amountToWithdrawLevel1 = amountToWithdrawLevel1.add(lpTokens);
@@ -201,23 +266,29 @@ contract CascadeV2 is OwnableUpgradeSafe {
         }
     }
 
+    /**
+     * @notice Allows a user to upgrade their deposit-seconds multipler for the given deposits.
+     * @param deposits A list of the indices of deposits to be upgraded.
+     */
     function upgradeMultiplierLevel(uint256[] memory deposits)
-        public
+        external
     {
+        require(deposits.length > 0, "no deposits");
+
         updateDepositSeconds(msg.sender);
 
         for (uint256 i = 0; i < deposits.length; i++) {
             uint256 idx = deposits[i];
-            uint256 age = now.sub(userDeposits_depositTimestamp[msg.sender][idx]);
+            uint256 age = now.sub(userDepositsDepositTimestamp[msg.sender][idx]);
 
-            if (age <= 30 days) {
+            if (age <= 30 days || userDepositsMultiplierLevel[msg.sender][idx] == 3) {
                 continue;
             }
 
-            uint8 oldLevel = userDeposits_multiplierLevel[msg.sender][idx];
-            uint256 tokensDeposited = userDeposits_numLPTokens[msg.sender][idx];
+            uint8 oldLevel = userDepositsMultiplierLevel[msg.sender][idx];
+            uint256 tokensDeposited = userDepositsNumLPTokens[msg.sender][idx];
 
-            if (age > 30 days && userDeposits_multiplierLevel[msg.sender][idx] == 1) {
+            if (age > 30 days && userDepositsMultiplierLevel[msg.sender][idx] == 1) {
                 uint256 secondsSinceLevel2 = age - 30 days;
                 uint256 extraDepositSeconds = tokensDeposited.mul(secondsSinceLevel2);
                 totalDepositedLevel1 = totalDepositedLevel1.sub(tokensDeposited);
@@ -227,10 +298,10 @@ contract CascadeV2 is OwnableUpgradeSafe {
                 userTotalLPTokensLevel1[msg.sender] = userTotalLPTokensLevel1[msg.sender].sub(tokensDeposited);
                 userTotalLPTokensLevel2[msg.sender] = userTotalLPTokensLevel2[msg.sender].add(tokensDeposited);
                 userDepositSeconds[msg.sender] = userDepositSeconds[msg.sender].add(extraDepositSeconds);
-                userDeposits_multiplierLevel[msg.sender][idx] = 2;
+                userDepositsMultiplierLevel[msg.sender][idx] = 2;
             }
 
-            if (age > 60 days && userDeposits_multiplierLevel[msg.sender][idx] == 2) {
+            if (age > 60 days && userDepositsMultiplierLevel[msg.sender][idx] == 2) {
                 uint256 secondsSinceLevel3 = age - 60 days;
                 uint256 extraDepositSeconds = tokensDeposited.mul(secondsSinceLevel3);
                 totalDepositedLevel2 = totalDepositedLevel2.sub(tokensDeposited);
@@ -240,12 +311,21 @@ contract CascadeV2 is OwnableUpgradeSafe {
                 userTotalLPTokensLevel2[msg.sender] = userTotalLPTokensLevel2[msg.sender].sub(tokensDeposited);
                 userTotalLPTokensLevel3[msg.sender] = userTotalLPTokensLevel3[msg.sender].add(tokensDeposited);
                 userDepositSeconds[msg.sender] = userDepositSeconds[msg.sender].add(extraDepositSeconds);
-                userDeposits_multiplierLevel[msg.sender][idx] = 3;
+                userDepositsMultiplierLevel[msg.sender][idx] = 3;
             }
-            emit UpgradeMultiplierLevel(msg.sender, idx, oldLevel, userDeposits_multiplierLevel[msg.sender][idx], block.timestamp);
+            emit UpgradeMultiplierLevel(msg.sender, idx, oldLevel, userDepositsMultiplierLevel[msg.sender][idx], block.timestamp);
         }
     }
 
+    /**
+     * @notice Called by Cascade v1 to migrate funds into Cascade v2.
+     * @param user The user for whom to migrate funds.
+     * @param numLPTokens How many LP tokens to migrate.
+     * @param numRewardTokens How many BASE tokens to migrate.
+     * @param multiplier The user's current multiplier.
+     * @param depositTimestamp The timestamp of the user's v1 deposit.
+     * @param depositSeconds The user's current deposit-seconds.
+     */
     function migrate(
         address user,
         uint256 numLPTokens,
@@ -254,16 +334,20 @@ contract CascadeV2 is OwnableUpgradeSafe {
         uint256 depositTimestamp,
         uint256 depositSeconds
     )
-        public
+        external
     {
-        require(msg.sender == address(cascadeV1), "only cascade");
+        require(msg.sender == cascadeV1, "only cascade v1");
+        require(user != address(0x0), "zero address");
         require(numLPTokens > 0, "no stake");
+        require(multiplier > 0, "zero multiplier");
+        require(depositTimestamp > 0, "zero timestamp");
+        require(depositSeconds > 0, "zero seconds");
 
-        updateDepositSeconds(address(0x0));
+        updateDepositSeconds(user);
 
-        userDeposits_numLPTokens[user].push(numLPTokens);
-        userDeposits_multiplierLevel[user].push(multiplier);
-        userDeposits_depositTimestamp[user].push(depositTimestamp);
+        userDepositsNumLPTokens[user].push(numLPTokens);
+        userDepositsMultiplierLevel[user].push(multiplier);
+        userDepositsDepositTimestamp[user].push(depositTimestamp);
         userDepositSeconds[user] = depositSeconds;
         userLastAccountingUpdateTimestamp[user] = now;
         totalDepositSeconds = totalDepositSeconds.add(depositSeconds);
@@ -286,12 +370,17 @@ contract CascadeV2 is OwnableUpgradeSafe {
      * Accounting utilities
      */
 
+    /**
+     * @notice Updates the global deposit-seconds accounting as well as that of the given user.
+     * @param user The user for whom to update the accounting.
+     */
     function updateDepositSeconds(address user)
         public
     {
         (totalDepositSeconds, userDepositSeconds[user]) = getUpdatedDepositSeconds(user);
         lastAccountingUpdateTimestamp = now;
         userLastAccountingUpdateTimestamp[user] = now;
+        emit UpdateDepositSeconds(user, totalDepositSeconds, userDepositSeconds[user]);
     }
 
     function unlockedRewardShares(uint256 rewardsIdx)
@@ -332,6 +421,10 @@ contract CascadeV2 is OwnableUpgradeSafe {
      * Getters
      */
 
+    /**
+     * @notice Returns the global deposit-seconds as well as that of the given user.
+     * @param user The user for whom to fetch the current deposit-seconds.
+     */
     function getUpdatedDepositSeconds(address user)
         public
         view
@@ -351,11 +444,17 @@ contract CascadeV2 is OwnableUpgradeSafe {
         return (_totalDepositSeconds, _userDepositSeconds);
     }
 
+    /**
+     * @notice Returns the BASE rewards owed to the given user.
+     * @param user The user for whom to fetch the current rewards.
+     */
     function owedTo(address user)
         public
         view
         returns (uint256)
     {
+        require(user != address(0x0), "zero address");
+
         (uint256 totalDS, uint256 userDS) = getUpdatedDepositSeconds(user);
         if (totalDS == 0) {
             return 0;
@@ -363,6 +462,9 @@ contract CascadeV2 is OwnableUpgradeSafe {
         return rewardsPool().mul(userDS).div(totalDS);
     }
 
+    /**
+     * @notice Returns the total rewards pool.
+     */
     function rewardsPool()
         public
         view
@@ -375,6 +477,11 @@ contract CascadeV2 is OwnableUpgradeSafe {
         return sharesToTokens(totalShares);
     }
 
+    /**
+     * @notice Returns various statistics about the given user and deposit.
+     * @param user The user to fetch.
+     * @param depositIdx The index of the given user's deposit to fetch.
+     */
     function depositInfo(address user, uint256 depositIdx)
         public
         view
@@ -387,11 +494,13 @@ contract CascadeV2 is OwnableUpgradeSafe {
             uint256 _owed
         )
     {
+        require(user != address(0x0), "zero address");
+
         (_totalDepositSeconds, _userDepositSeconds) = getUpdatedDepositSeconds(user);
         return (
-            userDeposits_numLPTokens[user][depositIdx],
-            userDeposits_depositTimestamp[user][depositIdx],
-            userDeposits_multiplierLevel[user][depositIdx],
+            userDepositsNumLPTokens[user][depositIdx],
+            userDepositsDepositTimestamp[user][depositIdx],
+            userDepositsMultiplierLevel[user][depositIdx],
             _userDepositSeconds,
             _totalDepositSeconds,
             owedTo(user)
